593c5461f6a67b5cee354ff0da54df9d28a08ac5
Jessica Chen <jiangyuechen2024@u.northwestern.edu>
2024-05-31 23:50:02 +0000

    with some bugs for image upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ENTR-451 Homework #4
 
 Full instructions at https://entr451.com/homework-4/
+
+Live Render URL: https://hw4-xiud.onrender.com
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,42 @@
 class EntriesController < ApplicationController
+  # def index
+  #   @entries = Entry.all
 
+  #   # alternative responses to requests other than HTML
+  #   respond_to do |format|
+  #     format.html # implicitly renders posts/index.html.erb
+  #     format.json do
+  #       render :json => @entries 
+  #     end
+  #   end
+  # end
+  
   def new
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = @current_user["id"] 
-    @entry.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"] 
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@entry["place_id"]}"
   end
+  
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end  
 
 end
